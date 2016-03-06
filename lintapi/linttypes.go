2b86ef078db02d0429a04ba9bd6ca124a9b096ec
daveym <davey.mcglade@gmail.com>
2016-03-06 20:46:58 +0000

    More types added for response

--- lintapi/linttypes.go
+++ lintapi/linttypes.go
@@ -16,6 +16,8 @@ const (
 	RedirectURI string = "https://github.com/daveym/lint/blob/master/AUTHCOMPLETE.md"
 )
 
+type Time time.Time
+
 // Initial Authentication response from Pocket
 type authenticationResponse struct {
 	Code  string
@@ -37,7 +39,47 @@ type getListRequest struct {
 	DetailType  string
 	Search      string
 	Domain      string
-	Since       time.Time
+	Since       Time
 	Count       int
 	Offset      int
+}
+
+type ItemStatus int
+type ItemMediaAttachment int
+
+const (
+	ItemStatusUnread   ItemStatus = 0
+	ItemStatusArchived            = 1
+	ItemStatusDeleted             = 2
+)
+
+const (
+	ItemMediaAttachmentNoMedia  ItemMediaAttachment = 0
+	ItemMediaAttachmentHasMedia                     = 1
+	ItemMediaAttachmentIsMedia                      = 2
+)
+
+type listItem struct {
+	ItemID        int        `json:"item_id,string"`
+	ResolvedId    int        `json:"resolved_id,string"`
+	GivenURL      string     `json:"given_url"`
+	ResolvedURL   string     `json:"resolved_url"`
+	GivenTitle    string     `json:"given_title"`
+	ResolvedTitle string     `json:"resolved_title"`
+	Favorite      int        `json:",string"`
+	Status        ItemStatus `json:",string"`
+	Excerpt       string
+	IsArticle     int                 `json:"is_article,string"`
+	HasImage      ItemMediaAttachment `json:"has_image,string"`
+	HasVideo      ItemMediaAttachment `json:"has_video,string"`
+	WordCount     int                 `json:"word_count,string"`
+	Tags          map[string]map[string]interface{}
+	Authors       map[string]map[string]interface{}
+	Images        map[string]map[string]interface{}
+	Videos        map[string]map[string]interface{}
+	SortId        int  `json:"sort_id"`
+	TimeAdded     Time `json:"time_added"`
+	TimeUpdated   Time `json:"time_updated"`
+	TimeRead      Time `json:"time_read"`
+	TimeFavorited Time `json:"time_favorited"`
 }
