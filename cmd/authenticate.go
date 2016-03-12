80f1cd04bfa83060ab541576113b9a7615bc18dd
daveym <davey.mcglade@gmail.com>
2016-03-12 18:19:03 +0000

    AccessToken Persistance working

--- cmd/authenticate.go
+++ cmd/authenticate.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 
@@ -13,43 +14,53 @@ import (
 
 var authenticateCmd = &cobra.Command{
 	Use:   "auth",
-	Short: "Authenticate against Pocket using your consumer key. One off activity.",
+	Short: "Authenticate against Pocket  to get an Access Token. One off activity.",
 	Long: `To access the Pocket API, you need to authenticate with your consumer key. Your consumer key 
 	can be found under the development area within the pocket website`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		fmt.Println(viper.Get("AccessToken"))
+		if len(viper.GetString("AccessToken")) == 0 {
 
-		AuthNResp := &LintApi.AuthenticationResponse{}
-		err := LintApi.Authenticate(LintApi.ConsumerKey, AuthNResp)
+			AuthNResp := &LintApi.AuthenticationResponse{}
+			err := LintApi.Authenticate(LintApi.ConsumerKey, AuthNResp)
 
-		if err != nil {
-			fmt.Println("Please check your consumer key, it does not appear to be valid.")
-			return
+			if err != nil {
+				fmt.Println("Please check your consumer key, it does not appear to be valid.")
+				return
+			}
+
+			browser := exec.Command("open", LintApi.UserAuthorisationURL+
+				"request_token="+AuthNResp.Code+
+				"&redirect_uri="+LintApi.RedirectURI)
+
+			_, err = browser.Output()
+
+			if err != nil {
+				println("Error opening authentication page. Please check your connectivity/default browser.")
+				return
+			}
+
+			fmt.Println("Press ENTER when you have authorised Lint to access to Pocket.")
+			bufio.NewReader(os.Stdin).ReadBytes('\n')
+
+			AuthRResp := &LintApi.AuthorisationResponse{}
+			err = LintApi.Authorise(LintApi.ConsumerKey, AuthNResp.Code, AuthRResp)
+
+			if err != nil {
+				fmt.Println("Error authorising your consumer key and request token. Have you granted permission to Lint?")
+			}
+
+			cfgval := fmt.Sprintf("AccessToken: %v\nUsername: %v", AuthRResp.AccessToken, AuthRResp.Username)
+			err = ioutil.WriteFile(LintApi.CfgFile, []byte(cfgval), 0644)
+
+			viper.Set("AccessToken", AuthRResp.AccessToken)
+			viper.Set("Username", AuthRResp.Username)
+
+			println("Authentication Successful - Access Token is persisted to lint.yaml")
+
+		} else {
+			println("Already authenticated - Access Token present in lint.yaml")
 		}
-
-		browser := exec.Command("open", LintApi.UserAuthorisationURL+
-			"request_token="+AuthNResp.Code+
-			"&redirect_uri="+LintApi.RedirectURI)
-
-		_, err = browser.Output()
-
-		if err != nil {
-			println("Error opening authentication page. Please check your connectivity/default browser.")
-			return
-		}
-
-		fmt.Println("Press ENTER when you have authorised Lint to access to Pocket.")
-		bufio.NewReader(os.Stdin).ReadBytes('\n')
-
-		AuthRResp := &LintApi.AuthorisationResponse{}
-		err = LintApi.Authorise(LintApi.ConsumerKey, AuthNResp.Code, AuthRResp)
-
-		if err != nil {
-			fmt.Println("Error authorising your consumer key and request token. Have you granted permission to Lint?")
-		}
-
-		println(AuthRResp.AccessToken, AuthRResp.Username)
 	}}
 
 func init() {
