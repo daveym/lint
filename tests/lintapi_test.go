62da95e229d8876353903402d94c3fe2d3ae99f7
daveym <davey.mcglade@gmail.com>
2016-03-13 14:43:48 +0000

    Test not specific to Lint - commenting out untiul lint refactored for testability.

--- tests/lintapi_test.go
+++ tests/lintapi_test.go
@@ -1,14 +1,12 @@
 package hello
 
-import "testing"
-
-func TestHello(t *testing.T) {
+/*func TestHello(t *testing.T) {
 	expected := "Hello Go!"
 	actual := hello()
 	if actual != expected {
 		t.Error("Test failed")
 	}
-}
+}*/
 
 /*Authenticate(consumerKey string) (string, error)
 Authorise(consumerKey string, code string) (string, string, error)
