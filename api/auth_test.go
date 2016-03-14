937f0931eb32a5baba1ac3efdd897132a7ead7a8
daveym <davey.mcglade@gmail.com>
2016-03-14 08:38:50 +0000

    Restructuring tests

--- api/auth_test.go
+++ api/auth_test.go
@@ -3,19 +3,57 @@ package api
 import (
 	"testing"
 
+	"github.com/daveym/lint/api"
 	"github.com/daveym/lint/pocket"
 )
 
-func InvalidConsumerKey(t *testing.T) {
+func TestAuthValidConsumerKey(t *testing.T) {
 
-	pc := &pocket.MockClient{}
+	mc := &pocket.MockClient{}
 
-	t.Error("Test failed")
+	expectedmsg := "Please check your consumer key, it does not appear to be valid."
+	actualmsg := api.Authenticate(mc)
+
+	t.Fatal("TestValidConsumerKey failed")
 }
 
-func InvalidConsumerKey(t *testing.T) {
+func TestAuthInvalidConsumerKey(t *testing.T) {
 
-	pc := &pocket.MockClient{}
+	mc := &pocket.MockClient{}
 
-	t.Error("Test failed")
+	expectedmsg := "Please check your consumer key, it does not appear to be valid."
+	actualmsg := api.Authenticate(mc)
+
+	t.Fatal("TestInvalidConsumerKey failed")
+}
+
+func TestAuthBrowserPocketSuccess(t *testing.T) {
+
+	mc := &pocket.MockClient{}
+	actualmsg := api.AuthoriseUse(mc)
+
+	t.Fatal("TestBrowserPocketAuthSuccess failed")
+}
+
+func TestAuthBrowserPocketFail(t *testing.T) {
+
+	mc := &pocket.MockClient{}
+	actualmsg := api.AuthoriseUse(mc)
+
+	t.Fatal("TestBrowserPocketAuthFail failed")
+}
+
+func TestAuthGetAccessTokenSuccess(t *testing.T) {
+
+	mc := &pocket.MockClient{}
+	actualmsg := api.GetAccessToken(mc)
+
+	t.Fatal("TestGetAccessTokenSuccess failed")
+}
+
+func TestAuthGetAccessTokenFail(t *testing.T) {
+
+	mc := &pocket.MockClient{}
+	actualmsg := GetAccessToken(mc)
+	t.Fatal("TestGetAccessTokenFail failed")
 }
